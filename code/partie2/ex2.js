6dec412555f4fde3cf9814a047ad23f19022dd51
Arnaud Geiser <arnaudgeiser@gmail.com>
2019-05-29 18:17:04 +0200

    Added ex1 and ex2

---
+++ code/partie2/ex2.js
@@ -0,0 +1,147 @@
+const people = [
+    {
+        id: 1,
+        name: "Arnaud",
+        age: 29
+    },
+    {
+        id: 2,
+        name: "Xavier",
+        age: 22
+    },
+    {
+        id: 3,
+        name: "Robert",
+        age: 50
+    },
+    {
+        id: 4,
+        name: "Damien",
+        age : 37
+    },
+    {
+        id: 5,
+        name: "Jerome",
+        age : 90
+    },
+    {
+        id: 6,
+        name: "Dominique",
+        age : 40
+    }
+]
+
+const movies = [
+    {
+        title: "Lord of the Rings",
+        author: "Peter Jackson",
+        comments: [{peopleId: 1, grade: 5},
+                   {peopleId: 2, grade: 7},
+                   {peopleId: 3, grade: 4},
+                   {peopleId: 4, grade: 9}]
+    },
+    {
+        title: "The Empire strikes back",
+        author: "Steven Spielberg",
+        comments: [{peopleId: 1, grade: 2},
+                   {peopleId: 2, grade: 8},
+                   {peopleId: 3, grade: 10},
+                   {peopleId: 4, grade: 7}]
+    },
+]
+
+Array.prototype.flat = function() { 
+    return this.reduce((acc,v) => acc.concat(v),[]) 
+};
+
+/**
+ * Applies a function (f) to each element of an array (arr).
+ * Type: [A] => (A => B) => [B]
+ * 
+ * TODO: 
+ * 1. I can pass an array and a function to apply a transformation via the map() function
+ */
+export function map(arr, f) {
+    let tmp = [];
+    for(let c of arr) {
+        tmp.push(f(c));
+    }
+    return tmp;
+}
+
+
+
+/**
+ * Filters the elements of an array (xs) keeping only those who match a given predicate (p).
+ * Type: [A] => (A => Boolean) => [A]
+ * 
+ * TODO:
+ * 2. I can pass an array and a predicate to filter out items via the filter() function
+ */
+export function filter(arr, p) {
+    let tmp = [];
+    for(let c of arr) {
+        if(p(c)) {
+            tmp.push(c);
+        }
+    }
+    return tmp;
+}
+
+/**
+ * Returns a person by its id
+ * Type: Int => Person
+ * 
+ * TODO:
+ * 3. I can create a function that close over a variable in its environnement, that\' called a closure!
+ */
+export function findPersonById(id) {
+    return filter(people, p => p.id===id)[0];
+}
+
+/**
+ * Returns the people younger than 30
+ * Type: Unit => [Person]
+ * 
+ * TODO:
+ * 4. I can get people younger than 30'
+ */
+export function peopleYoungerThan30() {
+    return filter(people, p => p.age < 30);
+}
+
+/**
+ * Returns the people acronyms
+ * Type: Unit => [String]
+ * Example: ["Ar", "Xa", Da"]
+ * 
+ * TODO:
+ * 5. I can get two letters acronyms
+ */
+export function peopleAcronyms() {
+    return map(people, p => p.name.substr(0,2));
+}
+
+/**
+ * Returns the people acronyms in uppercase that starts with the letter "D"
+ * Type : Unit -> [String]
+ * Example : ["DA", "DO"]
+ * 
+ * TODO:
+ * 6. I can get two letters acronyms in uppercase that starts with D
+ */
+export function peopleAcronymsUppercaseStartWithD() {
+    return filter(map(map(people, p => p.name.substr(0,2)), p => p.toUpperCase()), p => p[0]=='D');
+}
+
+/**
+ * Returns the comments for each user by movie
+ * Type : Unit -> [{movie, name, grade}]
+ * Example : [{name: "Lord of the Rings", name: "Xavier", grade: 5},{name: "Empire strikes back", name: "Arnaud", grade: 6}]
+ * 
+ * TODO:
+ * 7. I can get all comments for each movie and people
+ */
+export function commentsByMovie() {
+    return map(movies, m => map(m.comments, c => ({movie: m.title, name: findPersonById(c.peopleId).name, grade: c.grade}))).flat();
+}
--- code/partie2/test.js
+++ code/partie2/test.js
@@ -1,24 +1,66 @@
-import { double, doubleArray, addCurry } from './partie2';
+import { 
+  double, 
+  doubleArray,
+  add5 }
+from './ex1';
+
+import { 
+  findPersonById,
+  peopleAcronyms, 
+  peopleAcronymsUppercaseStartWithD,
+  peopleYoungerThan30,
+  commentsByMovie, 
+  map,
+  filter} 
+from './ex2';
 
 const chai = require('chai');
 const should = chai.should();
 
 describe('First-class functions', function() {
-  it('1. You can store a function in a variable', function() {
+  it('1. I can store a function in a variable', function() {
     should.not.equal(double, null, "double ne doit pas être null");
     double(2).should.equal(4);
   });
-  it('2. You can pass a function to another function', function() {
+  it('2. I can pass a function to another function as parameter', function() {
     doubleArray([1,2,4], (x) => x * 2).should.deep.equal([2,4,8]);
   });
 })
 
 describe('Higher-order functions', function() {
-  it('1. You can create a function from a function', function() {
-    should.not.equal(double, null, "double ne doit pas être null");
-    double(2).should.equal(4);
-  });
-  it('2. You can create a function from a function', function() {
-    addCurry(5)(2)(7).should.equal(14);
+  it('1. I can create a function from a function', function() {
+    should.not.equal(add5, null, "add5 ne doit pas être null");
+    add5(2).should.equal(7);
   });
 });
+ 
+describe('Movies et peoples', function() {
+  it('1. I can pass an array and a function to apply a transformation via the map() function', function() {
+    map([1,2,3,4], e => e * 2).should.deep.equal([2,4,6,8]);
+  });
+  it('2. I can pass an array and a predicate to filter out items via the filter() function', function() {
+    filter([1,2,3,4], e => e % 2 == 0).should.deep.equal([2,4]); 
+  });
+  it('3. I can create a function that close over a variable in its environnement, that\' called a closure!', function() {
+    should.not.equal(findPersonById(1), null, "Not implemented");
+    findPersonById(1).name.should.equal("Arnaud");
+  });
+  it('4. I can get people younger than 30', function() {
+    peopleYoungerThan30().length.should.equal(2);
+  });
+  it('5. I can get two letters acronyms', function() {
+    peopleAcronyms().every(e => {
+      e.length.should.equal(2)
+    });
+  })
+  it('6. I can get two letters acronyms in uppercase that starts with D', function() {
+    peopleAcronymsUppercaseStartWithD().length.should.equal(2);
+    peopleAcronymsUppercaseStartWithD().every(e => {
+      e.should.match(/^D[A-Z]$/)
+    });
+  })
+  it('7. I can get all comments for each movie and people', function() {
+    commentsByMovie().forEach(c => c.should.have.all.keys('name','grade','movie'));
+    commentsByMovie().length.should.equal(8);
+  });
+}); 
