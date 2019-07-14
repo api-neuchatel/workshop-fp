b3d334587163e10439d2b4a341a81a3e184f7b63
xavierpantet <25949462+xavierpantet@users.noreply.github.com>
2019-07-14 23:13:42 +0200

    Finished live coding part 2

---
+++ course/2. Abstraction + Closures/live/test.js
@@ -0,0 +1,27 @@
+import { divisibleBy2, triple } from '././base';
+
+const chai = require('chai');
+const should = chai.should();
+  
+describe('DivisibleBy2', function() {
+  it('Should return empty array on empty input', function() {
+    divisibleBy2([]).should.deep.equal([])
+  })
+  it('Should return only even elements of random input', function() {
+    let random = Array(40).fill().map(() => Math.round(Math.random() * 40))
+    divisibleBy2(random).every(x => x % 2 === 0)
+  })
+})
+
+describe('Triple', function() {
+  it('Should return empty array on empty input', function() {
+    triple([]).should.deep.equal([])
+  })
+  it('Should triple every element of a random input', function() {
+    let random = Array(40).fill().map(() => Math.round(Math.random() * 40))
+    let tripled = triple(random)
+    for(var i = 0; i < 40; i++) {
+      tripled[i].should.equal(3*random[i])
+    }
+  })
+})
