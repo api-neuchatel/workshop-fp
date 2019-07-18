81374ed478e5ed8a8b6dbb2fa183752f9fb98e67
xavierpantet <25949462+xavierpantet@users.noreply.github.com>
2019-07-18 08:26:13 +0200

    Review live coding 3

--- course/3. Currying + Lazy Evaluation/live/test.js
+++ course/3. Currying + Lazy Evaluation/live/test.js
@@ -1,4 +1,4 @@
-import { greet, Stream } from './final';
+import { greet, Stream } from './base';
 
 const chai = require('chai');
 const should = chai.should();
@@ -10,18 +10,5 @@ describe('Greet', function() {
 })
 
 describe('Streams', function() {
-  let stream = Stream.init(0)(x => x + 1)
-
-  it('take(0) should return empty array', function() {
-    Stream.take(stream)(0).should.deep.equal([])
-  })
-
-  it('take should take the first n elements of the stream', function() {
-    Stream.take(stream)(5).should.deep.equal([0, 1, 2, 3, 4])
-  })
-
-  it('map should apply function to every element of a stream', function() {
-    let mapped = Stream.map(stream)(x => 2*x)
-    Stream.take(mapped)(5).should.deep.equal([0, 2, 4, 6, 8])
-  })
+  
 })
